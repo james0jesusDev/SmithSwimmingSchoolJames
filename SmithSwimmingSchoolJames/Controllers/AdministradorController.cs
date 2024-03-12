e707d4177dee34118db0cb222c628a7145ec1ce2
James Andrade Cevallos <james0jesus@gmail.com>
2024-03-12 11:10:13 +0100

    Creador de añadir , editar y ver administradores

---
+++ SmithSwimmingSchoolJames/Controllers/AdministradorController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmithSwimmingSchoolJames.Data;
+using SmithSwimmingSchoolJames.Models;
+
+namespace SmithSwimmingSchoolJames.Controllers
+{
+
+    [Authorize(Roles = "Admin,Coach")]
+
+    public class AdministradorController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public AdministradorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+
+
+
+        public async Task <IActionResult> Index()
+        {
+
+            ViewData["Registrado"] = false;
+
+            if (User.Identity.Name != null)
+            {
+                var admin = await _db.Administradors.FirstOrDefaultAsync(i => i.AdministradorUser == User.Identity.Name);
+
+                if (admin != null)
+                {
+                    ViewBag.AdministradorId = admin.AdministratorId;
+                    ViewData["Registrado"] = true;
+                }
+            }
+
+            return View();
+        }
+
+        [Authorize(Roles ="Admin")]
+    public async Task<IActionResult> AllProfiles()
+        {
+            var admin = await _db.Administradors.ToListAsync();
+            return View(admin);
+
+
+        }
+
+
+        public IActionResult AddProfile()
+
+        { 
+            return View(); 
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> AddProfile(Administrador administrador)
+        {
+            _db.Add(administrador);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("AllProfiles");
+
+
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> EditProfile(int id)
+        {
+            var adminToUpadate = await _db.Administradors.FirstOrDefaultAsync(i => i.AdministratorId == id);
+            return View(adminToUpadate);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(Administrador admin)
+        {
+            _db.Update(admin);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("AllProfiles");
+        }
+
+
+
+
+    }
+}
--- SmithSwimmingSchoolJames/Controllers/HomeController.cs
+++ SmithSwimmingSchoolJames/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SmithSwimmingSchoolJames.Models;
 using System.Diagnostics;
@@ -12,7 +13,7 @@ namespace SmithSwimmingSchoolJames.Controllers
         {
             _logger = logger;
         }
-
+        [Authorize]
         public IActionResult Index()
         {
             return View();
